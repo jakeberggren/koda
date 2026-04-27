7e3fd8dc8e641f8c8534546bda78506a1c6cca9e
Jakob Berggren <jakob@jakobberggren.com>
2026-04-27 22:51:08 +0200

    refactor: remove ListDirectoryTool from builtins
    Removes listdirectory from the builtin tools since the
    same capabilities are now available via the new bash
    tool instead.

--- packages/koda/src/koda/tools/builtins/__init__.py
+++ packages/koda/src/koda/tools/builtins/__init__.py
@@ -2,7 +2,6 @@ from koda.tools.builtins.bash import BashTool
 from koda.tools.builtins.edit_file import EditFileTool
 from koda.tools.builtins.glob import GlobTool
 from koda.tools.builtins.grep import GrepTool
-from koda.tools.builtins.list_directory import ListDirectoryTool
 from koda.tools.builtins.read_file import ReadFileTool
 from koda.tools.builtins.write_file import WriteFileTool
 
@@ -11,7 +10,6 @@ __all__ = [
     "EditFileTool",
     "GlobTool",
     "GrepTool",
-    "ListDirectoryTool",
     "ReadFileTool",
     "WriteFileTool",
 ]
